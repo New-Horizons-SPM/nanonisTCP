0d63bda4cea639765a55994191dbc60e68b76704
Julian <jdceddia@gmail.com>
2022-05-03 19:15:35 +1000

    Typo Fixes
    Typo fixes in SafeTip module

--- nanonisTCP/SafeTipTest.py
+++ nanonisTCP/SafeTipTest.py
@@ -16,7 +16,7 @@ def run_test(TCP_IP='127.0.0.1', TCP_PORT=6502, make_plot=False):
     # Listening Port: see Nanonis File>Settings>TCP Programming Interface
     NTCP = nanonisTCP(TCP_IP, TCP_PORT)                                         # Nanonis TCP interface
     
-    safeTip = SafeTip(NTCP)                                                     # Nanonis Z-Controller Module
+    safeTip = SafeTip(NTCP)                                                     # Nanonis Safe Tip Module
     
     try:
         """
