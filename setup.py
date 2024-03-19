bd2dcc76fbe94ef01137cb80b2b0002da94903f6
ceds92 <julian.ceddia@monash.edu>
2024-03-19 18:17:34 +1100

    Change version to match pypi
    can now pip install nanonisTCP
    https://pypi.org/project/nanonisTCP/1.0.2/

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-version = '1.0.0'
+version = '1.0.2'
 
 setup(
     name='nanonisTCP',
